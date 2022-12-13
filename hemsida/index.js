4cd33ae276de490a4623f891ce799ba26b8d7144
saganorth <113887414+saganorth@users.noreply.github.com>
2022-12-13 22:15:45 +0100

    wsadjkd

--- hemsida/index.js
+++ hemsida/index.js
@@ -1,3 +1,5 @@
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +28,30 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+$(document).ready(function() {
+
+  $('.link').on('click', function(e){
+    
+    var hash = $(this).attr('href');
+     $('html,body').animate({
+       scrollTop: $(hash).offset().top
+     }, 800);
+    
+    return false;
+  });
+});
+
+
+
+function onScroll() {
+  if (window.pageYOffset) {
+    $$header.classList.add("is-active");
+  } else {
+    $$header.classList.remove("is-active");
+  }
+}
+
+const $$header = document.querySelector(".js-header");
+
+window.addEventListener("scroll", throttle(onScroll, 300));
